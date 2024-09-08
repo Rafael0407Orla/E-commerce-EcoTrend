13de1e4c0d86715aa04826cf0d484ff12648f425
Rafael Nascimento <rafaelpanhoca04@gmail.com>
2024-09-08 20:28:57 -0300

    feat: lendo json externo

--- Pages/Categoria/script.js
+++ Pages/Categoria/script.js
@@ -1,108 +1,30 @@
-const produtos = [
-  {
-    id: 1,
-    nome: "Camiseta de Algodão Orgânico",
-    preco: 89.9,
-    descricao:
-      "Camiseta feita de algodão 100% orgânico, suave ao toque e sustentável.",
-    tipo: "Roupas",
-    marca: "EcoWear",
-    categoria: "roupas",
-  },
-  {
-    id: 3,
-    nome: "Shampoo Natural com Óleo de Coco",
-    preco: 34.5,
-    descricao:
-      "Shampoo sem químicos agressivos, com óleo de coco e fragrância natural.",
-    tipo: "Beleza",
-    marca: "EcoWear",
-    categoria: "cuidados",
-  },
-  {
-    id: 6,
-    nome: "Toalhas de Algodão Orgânico",
-    preco: 99.9,
-    descricao:
-      "Toalhas super macias e feitas de algodão orgânico, ideal para o uso diário.",
-    tipo: "Casa",
-    marca: "EcoWear",
-    categoria: "casa",
-  },
-  {
-    id: 2,
-    nome: "Bolsa de Linho Reciclado",
-    preco: 129.9,
-    descricao:
-      "Bolsa estilosa e funcional feita de linho reciclado, com alças de fibra de bambu.",
-    tipo: "Acessórios",
-    marca: "GreenStyle",
-    categoria: "roupas",
-  },
-  {
-    id: 4,
-    nome: "Desodorante Vegano de Lavanda",
-    preco: 24.9,
-    descricao:
-      "Desodorante vegano e natural, com extrato de lavanda, livre de alumínio.",
-    tipo: "Cuidados Pessoais",
-    marca: "GreenStyle",
-    categoria: "cuidados",
-  },
-  {
-    id: 5,
-    nome: "Escova de Dentes de Bambu",
-    preco: 19.9,
-    descricao: "Escova de dentes ecológica feita de bambu biodegradável.",
-    tipo: "Cuidados Pessoais",
-    marca: "GreenStyle",
-    categoria: "cuidados",
-  },
-  {
-    id: 7,
-    nome: "Lâmpada LED de Baixo Consumo",
-    preco: 39.9,
-    descricao: "Lâmpada LED com alta durabilidade e baixo consumo de energia.",
-    tipo: "Tecnologia Verde",
-    marca: "EcoTech",
-    categoria: "casa",
-  },
-  {
-    id: 8,
-    nome: "Carregador Solar Portátil",
-    preco: 149.9,
-    descricao:
-      "Carregador portátil alimentado por energia solar, ideal para viagens.",
-    tipo: "Tecnologia Verde",
-    marca: "EcoTech",
-    categoria: "casa",
-  },
-  {
-    id: 9,
-    nome: "Copo Reutilizável de Fibra de Bambu",
-    preco: 24.9,
-    descricao:
-      "Copo leve e reutilizável, feito de fibra de bambu, ideal para o dia a dia.",
-    tipo: "Casa",
-    marca: "EcoTech",
-    categoria: "casa",
-  },
-];
+
+
+function getProducts() {
+  fetch("../../products.json")
+    .then((response) => response.json())
+    .then((data) => {
+      mostrarProdutosPorCategoria(data, categoria);
+      console.log(data);
+    })
+    .catch((error) => console.error("Error loading JSON file", error));
+}
 
 var categoria;
+var maxPrice;
 
 function getCategoriaFromURL() {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get("categoria");
 }
 
-function mostrarProdutosPorCategoria(
+function mostrarProdutosPorCategoria(data,
   categoria,
   precoMaximo = Infinity,
   marcaSelecionada = "Todas"
 ) {
   // Filtra produtos pela categoria
-  const produtosFiltrados = produtos.filter(
+  const produtosFiltrados = data.filter(
     (produto) => produto.categoria === categoria
   );
 
@@ -166,5 +88,5 @@ atualizarValorRange();
 
 addEventListener("DOMContentLoaded", function () {
   categoria = getCategoriaFromURL();
-  mostrarProdutosPorCategoria(categoria);
+  getProducts();
 });

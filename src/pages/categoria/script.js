3daa432121638dfc83999f587d088820ddec6634
Victor Rodriguez <victordz.dev@gmail.com>
2024-09-10 22:52:56 -0300

    fix

--- src/pages/categoria/script.js
+++ src/pages/categoria/script.js
@@ -94,3 +94,16 @@ addEventListener("DOMContentLoaded", function () {
   categoria = getCategoriaFromURL();
   getProducts();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const currentPath = window.location.pathname.split('/').pop(); // Obtém apenas o nome da página
+
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href').includes(currentPath)) {
+      link.classList.add('active');
+    }
+  });
+});
